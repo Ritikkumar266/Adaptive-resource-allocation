b667726064641de29694b27e7d06e07a69014b82
Ritik Kumar <152249518+Ritikkumar266@users.noreply.github.com>
2025-04-29 15:59:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,44 +3,34 @@ from resource_allocator import ResourceAllocator
 from scheduler import CPUScheduler
 from monitor import monitor_system
 from visualizer import live_plot
-
 import time
 import matplotlib.pyplot as plt
-
+import threading
 def main():
-    # Create simulated programs
     p1 = SimulatedProgram("Program-A")
     p2 = SimulatedProgram("Program-B")
     p3 = SimulatedProgram("Program-C")
-
     programs = [p1, p2, p3]
-
-    # Start programs
     for p in programs:
         p.start()
-
     allocator = ResourceAllocator(programs)
     scheduler = CPUScheduler(programs)
-
+    scheduler_thread = threading.Thread(target=scheduler.run)
+    scheduler_thread.daemon = True
+    scheduler_thread.start()
     cpu_history = []
     mem_history = []
-
-    plt.ion()  # Interactive mode for real-time plotting
-
     try:
-        for _ in range(5):  # Run for 20 cycles
+        for _ in range(5):
             allocator.allocate()
             cpu, mem = monitor_system()
             cpu_history.append(cpu)
             mem_history.append(mem)
-
             if len(cpu_history) > 50:
                 cpu_history.pop(0)
                 mem_history.pop(0)
-
             live_plot(cpu_history, mem_history)
             time.sleep(1)
-
     except KeyboardInterrupt:
         print("Stopping programs...")
     finally:
@@ -49,6 +39,5 @@ def main():
         for p in programs:
             p.join()
         print("All programs stopped.")
-
 if __name__ == "__main__":
     main()
