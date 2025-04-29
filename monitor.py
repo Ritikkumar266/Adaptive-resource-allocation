c2373d41208d511f0921029cfd71db0e3f49dfa3
Ritik Kumar <152249518+Ritikkumar266@users.noreply.github.com>
2025-04-29 16:00:26 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,7 +1,7 @@
 import psutil
 
 def monitor_system():
-    cpu_percent = psutil.cpu_percent(interval=0.5)  # Capture CPU usage in %
+    cpu = psutil.cpu_percent(interval=0.1)
     mem = psutil.virtual_memory()
-    mem_percent = mem.percent  # Get memory usage in %
-    return cpu_percent, mem_percent
+    # Return percentage used instead of MB
+    return cpu, mem.percent
